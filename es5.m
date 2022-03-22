5f12fc04c00a037b0ebf199a2a688efe043be9f4
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2022-03-22 10:12:04 +0100

    Calculate partial results of alg2

--- es5.m
+++ es5.m
@@ -10,4 +10,8 @@ vex = a^2 - b^2;    % valore esatto
 % simulo esecuzione alg1 in F(10,5,L,U)
 a2 = vpa(a^2, 5)  % variable precision arithemtic (operaizione, cifre di mantissa)
 b2 = vpa(b^2, 5)
-ris1 = vpa(1.0-1.9952, 5)
+ris1 = vpa(1.0-1.9952, 5) % -0.9952
+
+% simulo esecuzione alg2 in F(10,5,L,U)
+apb = vpa(a+b, 5)
+amb = vpa(a-b, 5)
