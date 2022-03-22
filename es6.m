c0410fdb31ffb08d41bc4b5a96f72d3c11a84c7c
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2022-03-22 10:46:51 +0100

    Calulate theoric realtive error

--- es6.m
+++ es6.m
@@ -7,4 +7,7 @@ x=10.^p;
 Ax = 1./x-1./(x+1); % val calcolato
 Bx = 1./(x.*(x+1)); % val esatto
 
-Erel = abs(Ax-Bx)./abs(Bx) % err relativo calcolato da matlab
+Erel = abs(Ax-Bx)./abs(Bx); % err relativo calcolato da matlab
+
+u = eps/2;
+Erel_teorico = (abs(x+1) + abs(x) + 1)*u;
