d0f12124ec9de266b3a279ee005150acc87383ff
Enrico Tagliaferri - enrico.tagliaferri2@studio.unibo.it <enrico.tagliaferri2@studio.unibo.it>
2022-03-22 10:10:31 +0100

    Calculate final result of alg1

--- es5.m
+++ es5.m
@@ -10,3 +10,4 @@ vex = a^2 - b^2;    % valore esatto
 % simulo esecuzione alg1 in F(10,5,L,U)
 a2 = vpa(a^2, 5)  % variable precision arithemtic (operaizione, cifre di mantissa)
 b2 = vpa(b^2, 5)
+ris1 = vpa(1.0-1.9952, 5)
